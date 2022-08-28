56f11592108711afba09281eda9454959f1884c0
laryssa rayanne <83220485+laryssa83010@users.noreply.github.com>
2022-08-28 16:50:30 -0300

    Add files via upload

---
+++ atualizacaodo Nis.py
@@ -0,0 +1,32 @@
+for c in range(1, 2):
+     c = float(input("Inform the NIS , only the number: "))
+     print('NIS update {} was successful !'.format(c))
+
+
+for c in range(1, 2):
+     c = float(input("Enter the CPF, only the number: "))
+     print('your CPF {} has been updated!'.format(c))
+
+
+     Mother = input("Enter mother´s name: ")
+     print('mother´s name is:', Mother, ',has been updated')
+
+
+     Father = input("Enter father´s name: ")
+     print('your father´s name is:', Father, ',has been updated')
+
+for c in range(1, 2):
+   c = float(input("Enter the number of the work card:"))
+   print('the work card number {} has been updated!'.format(c))
+
+for c in range(1, 2):
+   c = float(input("Enter voter registration number:"))
+   print('the voter registration number {} has been updated!'.format(c))
+
+for c in range(1, 2):
+   c = float(input("Enter birth certificate number:"))
+   print('birth certificate number {} has been updated!'.format(c))
+
+for c in range(1, 2):
+   c = float(input("Enter the number of the marriage certificate,if married,if not,enter 0:"))
+   print('registration updated successfully!')
